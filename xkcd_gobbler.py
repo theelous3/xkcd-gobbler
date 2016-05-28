f718df4f128ef799108575c38721a1c8de6755eb
theelous3 <theegrandmaster@gmail.com>
2016-05-28 15:29:58 +0100

    refactored status printer, some pep8ing

--- xkcd_gobbler.py
+++ xkcd_gobbler.py
@@ -30,7 +30,7 @@ class Scraper:
     
         try:
             img_url = soup.find('div', {'id':'comic'}).img['src']
-            if not os.path.isfile('Comics/'+img_url.split('/')[4]):
+            if not os.path.isfile(os.path.join('Comics', img_url.split('/')[4])):
                 if self.save_image(img_url):
                     self.success += 1
                 else:
@@ -44,11 +44,11 @@ class Scraper:
             self.skipped += 1
         self.terminal_knowledge()
 
-    def save_image(self, url):
-        image = requests.get('http:' + url, stream=True)
+    def save_image(self, img_url):
+        image = requests.get('http:' + img_url, stream=True)
 
         try:
-            with open('Comics/'+url.split('/')[4], 'wb') as image_file:
+            with open(os.path.join('Comics', img_url.split('/')[4]), 'wb') as image_file:
                 for bytechunk in image.iter_content():
                     image_file.write(bytechunk)
             return True
@@ -58,8 +58,12 @@ class Scraper:
     
     
     def terminal_knowledge(self):
-        sys.stdout.write('\r Sucessful:{} Failed:{} Skipped:{}'.format(self.success, self.failure, self.skipped))
-        sys.stdout.flush()
+        print('\r Sucessful:{} Failed:{} Skipped:{}'.format(self.success,
+                                                            self.failure, 
+                                                            self.skipped), 
+                                                                flush=True,
+                                                                end ='')
+
     
     
     def get_comic_count(self):
@@ -68,28 +72,28 @@ class Scraper:
         soup = BeautifulSoup(rc, "html.parser")
         num_finder = re.compile('Permanent link to this comic: http://xkcd.com/')
         get_num = soup.find(text=num_finder)
-        latest_comic_num = get_num.split('/')[3]
-        url_list = [('http://xkcd.com/' + str(i) + '/') for i in range(1, int(latest_comic_num)+1)]
-        return (latest_comic_num, url_list)
+        last_comic = get_num.split('/')[3]
+        url_list = [('http://xkcd.com/' + str(i)) for i in range(1, int(last_comic)+1)]
+        return (last_comic, url_list)
     
     
-    def get_parameters(self):
+    def main(self):
         latest_data = self.get_comic_count()
-        comics_available, url_list = latest_data[0], latest_data[1]
+        last_comic, url_list = latest_data
         
         print('Examples:')
         print('  Range-      "r: 50-100"')
         print('  Multi-      "m: 41,154,1002"')
-        print('  Individual- "i: {}"\n'.format(comics_available))
+        print('  Individual- "i: {}"\n'.format(last_comic))
 
-        user_requested_range = input('Which of the {} comic(s) would you like to download?\n'.format(comics_available))
+        usr_range = input('Which of the {} comic(s) would you like to download?\n'.format(last_comic))
         
         print()
         print('Checking/Creating "Comics" directory...')
 
         os.makedirs('Comics', exist_ok=True)
 
-        choice_type, choice_value = user_requested_range.split(':')
+        choice_type, choice_value = usr_range.split(':')
         if choice_type.lower() == 'r':
             choice_value = choice_value.split('-')
             url_list = url_list[int(choice_value[0])-1:int(choice_value[1])]
@@ -115,12 +119,12 @@ class Scraper:
             for i in self.failed_list:
                 print(i)
         if self.skipped_list:
-            print('Skipped:')    
+            print('Skipped:')
             for i in self.skipped_list:
                 print(i)
             print()
 
 if __name__ == "__main__":
     s = Scraper()
-    s.get_parameters()
+    s.main()
     
